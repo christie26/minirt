8c217a1722ea0d1fce87c6ebb8fbd3d7a67f42b2
Minju Kim <minkim3@c4r6s7.42seoul.kr>
2023-06-26 21:07:52 +0900

    diffuse light

--- src/utils/sphere_utils.c
+++ src/utils/sphere_utils.c
@@ -18,15 +18,15 @@ int hit_sphere(t_sphere sphere, t_ray ray)
 
 t_coordinate get_sphere_point(t_sphere sphere, t_ray ray)
 {
-	// t_coordinate	hit_point_1;
-	// t_coordinate	hit_point_2;
+	t_coordinate	hit_point_1;
+	t_coordinate	hit_point_2;
 	t_vector		dif;
 	double			a;
 	double			b;
 	double			c;
 	double			t_1;
 	double			t_2;
-	double 			t;
+
 
 	dif = get_vector_two_point(ray.origin, sphere.center);
 	a = vector_dot(ray.direction, ray.direction);
@@ -34,18 +34,9 @@ t_coordinate get_sphere_point(t_sphere sphere, t_ray ray)
 	c = vector_dot(dif, dif) - pow(sphere.diameter / 2, 2);
 	t_1 = quadratic_equation_1(a, b, c);
 	t_2 = quadratic_equation_2(a, b, c);
-	printf("t_1: %f\n", t_1);
-	printf("t_2: %f\n", t_2);
-	if (t_1 < 0 && t_2 < 0)
-		return (ray.origin);
-	if (t_1 < t_2)
-		t = t_1;
-	else
-		t = t_2;
-	return (calculate_hit_point(ray, t));
-	// hit_point_1 = calculate_hit_point(ray, t_1);
-	// hit_point_2 = calculate_hit_point(ray, t_2);
-	// if (get_distance(ray.origin, hit_point_1) < get_distance(ray.origin, hit_point_2))
-	// 	return(hit_point_1);
-	// return (hit_point_2);
+	hit_point_1 = calculate_hit_point(ray, t_1);
+	hit_point_2 = calculate_hit_point(ray, t_2);
+	if (get_distance(ray.origin, hit_point_1) < get_distance(ray.origin, hit_point_2))
+		return(hit_point_1);
+	return (hit_point_2);
 }
