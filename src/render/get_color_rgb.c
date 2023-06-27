82cf9d34ffb470ea8001e061652d98d3e64f15c3
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-06-27 13:14:16 +0900

    apply diffuse light

--- src/render/get_color_rgb.c
+++ src/render/get_color_rgb.c
@@ -13,16 +13,15 @@ static t_color	ray_sky_color(t_ray ray)
 	return (color_rgb);
 }
 
+// 요 함수가 이상한 듯..! ㅇ
 static t_color	get_color_sphere(t_sphere sphere, t_light light, t_coordinate hit_point)
 {
 	t_color		color_rgb;
 	t_vector	light_vector;
 	double		light_ratio;
 
-	light_vector = vector_unit(get_vector_two_point(hit_point, light.coordinate));
+	light_vector = vector_unit(get_vector_two_point(light.coordinate, hit_point));
 	light_ratio = vector_dot(light_vector, sphere.normal);
-	printf("light_ratio: %f\n", light_ratio);
-	// light_ratio *= -1;
 	if (light_ratio < 0)
 		return (sphere.color);
 	light_ratio = light_ratio * light.brightness;
