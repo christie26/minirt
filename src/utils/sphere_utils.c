63b8991df8263bfc9a34d8419358cdd5cd0316b6
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-06-27 13:26:03 +0900

    refactor : split get_sphere_point function

--- src/utils/sphere_utils.c
+++ src/utils/sphere_utils.c
@@ -16,27 +16,36 @@ int hit_sphere(t_sphere sphere, t_ray ray)
 	return (discriminant > 0);
 }
 
+static t_coordinate	get_closer_point(t_coordinate hit_point_1, t_coordinate hit_point_2, t_ray ray)
+{
+	if (get_distance(ray.origin, hit_point_1) < get_distance(ray.origin, hit_point_2))
+		return(hit_point_1);
+	return (hit_point_2);
+}
+
+static t_coordinate get_closer_hit_point(double a, double b, double c, t_ray ray)
+{
+	double t_1;
+	double t_2;
+	t_coordinate closer_hit_point;
+
+	t_1 = quadratic_equation_1(a, b, c);
+	t_2 = quadratic_equation_2(a, b, c);
+	closer_hit_point = get_closer_point(calculate_hit_point(ray, t_1), calculate_hit_point(ray, t_2), ray);
+	return (closer_hit_point);
+}
+
 t_coordinate get_sphere_point(t_sphere sphere, t_ray ray)
 {
-	t_coordinate	hit_point_1;
-	t_coordinate	hit_point_2;
 	t_vector		dif;
 	double			a;
 	double			b;
 	double			c;
-	double			t_1;
-	double			t_2;
 
 
 	dif = get_vector_two_point(ray.origin, sphere.center);
 	a = vector_dot(ray.direction, ray.direction);
 	b = 2 * vector_dot(dif, ray.direction);
 	c = vector_dot(dif, dif) - pow(sphere.diameter / 2, 2);
-	t_1 = quadratic_equation_1(a, b, c);
-	t_2 = quadratic_equation_2(a, b, c);
-	hit_point_1 = calculate_hit_point(ray, t_1);
-	hit_point_2 = calculate_hit_point(ray, t_2);
-	if (get_distance(ray.origin, hit_point_1) < get_distance(ray.origin, hit_point_2))
-		return(hit_point_1);
-	return (hit_point_2);
+	return (get_closer_hit_point(a, b, c, ray));
 }
