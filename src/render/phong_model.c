d15db0ba46816430a1471fd31f38e002dae74070
Minju Kim <minkim3@c1r3s5.42seoul.kr>
2023-06-28 15:34:58 +0900

    fix phong color

--- src/render/phong_model.c
+++ src/render/phong_model.c
@@ -1,17 +1,18 @@
 #include "../../includes/minirt.h"
 
-static t_color get_ambient_color(t_ambient ambient)
+static t_color	get_ambient_color(t_ambient ambient)
 {
-	t_color ambient_color;
+	t_color	ambient_color;
 
 	ambient_color = apply_brightness(ambient.color, ambient.ratio);
 	return (ambient_color);
 }
 
-static t_color get_diffuse_color(t_color obj_color, t_light light, double light_ratio)
+static t_color	get_diffuse_color(t_color obj_color, t_light light,
+		double light_ratio)
 {
-	t_color diffuse_color;
-	int	scala;
+	t_color	diffuse_color;
+	double		scala;
 
 	scala = light_ratio * light.brightness;
 	diffuse_color = apply_brightness(obj_color, scala);
@@ -25,7 +26,8 @@ static double	get_light_ratio(t_data data, t_coordinate hit_point)
 	t_vector	normal;
 
 	hit_to_light.origin = hit_point;
-	hit_to_light.direction = vector_unit(get_vector_two_point(hit_point, data.light.coordinate));
+	hit_to_light.direction = vector_unit(get_vector_two_point(hit_point,
+				data.light.coordinate));
 	fix_hit_to_light(&hit_to_light);
 	if (is_shadow(data, hit_to_light))
 		return (0);
@@ -34,9 +36,9 @@ static double	get_light_ratio(t_data data, t_coordinate hit_point)
 	return (light_ratio);
 }
 
-static t_color add_all_phong_colors(t_phong phong)
+static t_color	add_all_phong_colors(t_phong phong)
 {
-	t_color phong_color;
+	t_color	phong_color;
 
 	phong_color = add_color(phong.ambient_color, phong.diffuse_color);
 	return (phong_color);
@@ -44,15 +46,16 @@ static t_color add_all_phong_colors(t_phong phong)
 
 t_color	apply_phong_model(t_data data, t_coordinate hit_point)
 {
-	t_color		color_rgb;
-	double		light_ratio;
-	t_phong		phong;
+	t_color	color_rgb;
+	double	light_ratio;
+	t_phong	phong;
 
 	light_ratio = get_light_ratio(data, hit_point);
 	if (light_ratio < 0)
 		light_ratio = 0;
 	phong.ambient_color = get_ambient_color(data.ambient);
-	phong.diffuse_color = get_diffuse_color(data.sphere.color, data.light, light_ratio);
+	phong.diffuse_color = get_diffuse_color(data.sphere.color, data.light, \
+			light_ratio);
 	color_rgb = add_all_phong_colors(phong);
 	return (color_rgb);
 }
