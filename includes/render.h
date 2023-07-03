271a9e34acdea8d9fc727abad5c1cfa2120e995e
yoonsele <yoonseo9800@gmail.com>
2023-07-03 11:37:09 +0900

    shadow works, sphere diffused color works git status

--- includes/render.h
+++ includes/render.h
@@ -52,6 +52,8 @@ t_coordinate	get_hit_point(t_ray *ray, t_node *node);
 double			get_light_ratio(t_data data, t_coordinate hit_point, \
 					t_ray *ray);
 
+t_hit_sphere	hit_sphere(t_sphere sphere, t_ray ray);
+
 int is_parallel(t_vector vector1, t_vector vector2, double *dot_product);
 
 #endif
--- src/render/shadow_center.c
+++ src/render/shadow_center.c
@@ -1,5 +1,12 @@
 #include "../../includes/minirt.h"
 
+void	fix_hit_to_light(t_ray *hit_to_light)
+{
+	hit_to_light->origin.x = hit_to_light->origin.x + 0.1 *  hit_to_light->direction.x;
+	hit_to_light->origin.y = hit_to_light->origin.y + 0.1 *  hit_to_light->direction.y;
+	hit_to_light->origin.z = hit_to_light->origin.z + 0.1 *  hit_to_light->direction.z;
+}
+
 int	blocked_plane(t_plane plane, t_ray ray)
 {
 	t_vector	ray_to_plane;
@@ -7,7 +14,7 @@ int	blocked_plane(t_plane plane, t_ray ray)
 	double		t;
 
 	ray_to_plane = vector_sub(init_vector(ray.origin.x, ray.origin.y, ray.origin.z), \
-		init_vector(plane.coordinate.x, plane.coordinate.y, plane.coordinate.y));
+		init_vector(plane.coordinate.x, plane.coordinate.y, plane.coordinate.z));
 	if (is_parallel(plane.vector, ray.direction, &denominator))
 		return (0);
 	t = vector_dot(ray_to_plane, plane.vector) / denominator;
@@ -18,12 +25,12 @@ int	blocked_plane(t_plane plane, t_ray ray)
 
 int blocked_sphere(t_sphere sphere, t_ray ray)
 {
-    t_vector dif;
-	double	a;
-	double	b;
-	double	c;
-	double t_1;
-	double t_2;
+    t_vector	dif;
+	double		a;
+	double		b;
+	double		c;
+	double		t_1;
+	double		t_2;
 
 	dif = get_vector_two_point(sphere.center, ray.origin);
 	a = vector_dot(ray.direction, ray.direction);
@@ -36,31 +43,32 @@ int blocked_sphere(t_sphere sphere, t_ray ray)
 
 int	is_shadow(t_data data, t_ray hit_to_light)
 {
-	t_node *object;
-	t_coordinate hit_point;
+	t_node			*node;
+	void			*object;
+	t_coordinate	hit_point;
 
-	object = data.object_list->headnode;
+	node = data.object_list->headnode;
 	hit_point = hit_to_light.origin;
-
-	while (object)
+	while (node)
 	{
-		if (object->type == SPHERE)
+		object = node->object;
+		if (node->type == SPHERE)
 		{
 			if (blocked_sphere(*(t_sphere *)object, hit_to_light))
-			{
-				printf("blocked by sphere\n");
 				return (1);
-			}
+			// info = hit_sphere(*(t_sphere *)object, hit_to_light);
+			// if (info.discriminant > 0)
+				// return (1);
 		}
 		else
 		{
-			if (blocked_plane(*(t_plane *)object, hit_to_light))
-			{
-				printf("blocked by plane\n");
-				return (1);
-			}
+			// if (blocked_plane(*(t_plane *)object, hit_to_light))
+			// {
+			// 	printf("blocked by plane\n");
+			// 	return (1);
+			// }
 		}
-		object = object->next;
+		node = node->next;
 	}
 	return (0);
 }
