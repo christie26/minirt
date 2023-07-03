89b963a01d0726b360628d8c87d3e98b370af9bc
Minju Kim <minkim3@c4r5s6.42seoul.kr>
2023-07-03 11:17:05 +0900

    blocked sphere

--- src/render/shadow_center.c
+++ src/render/shadow_center.c
@@ -16,6 +16,24 @@ int	blocked_plane(t_plane plane, t_ray ray)
 	return (1);
 }
 
+int blocked_sphere(t_sphere sphere, t_ray ray)
+{
+    t_vector dif;
+	double	a;
+	double	b;
+	double	c;
+	double t_1;
+	double t_2;
+
+	dif = get_vector_two_point(sphere.center, ray.origin);
+	a = vector_dot(ray.direction, ray.direction);
+	b = 2 * vector_dot(dif, ray.direction);
+	c = vector_dot(dif, dif) - pow(sphere.diameter / 2, 2);
+	t_1 = quadratic_equation_1(a, b, c);
+	t_2 = quadratic_equation_2(a, b, c);
+	return (t_1 > 0 || t_2 > 0);
+}
+
 int	is_shadow(t_data data, t_ray hit_to_light)
 {
 	t_node *object;
