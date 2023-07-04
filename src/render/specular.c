ea018b03c99307f423df2f7cd09efc11d1a264d7
Minju Kim <minkim3@c3r2s4.42seoul.kr>
2023-07-04 14:44:16 +0900

    specular light

--- src/render/specular.c
+++ src/render/specular.c
@@ -9,17 +9,19 @@ t_color	get_specular_color(t_ray *ray, t_light light, double light_ratio)
 	double		specular_intensity;
 	t_color		specular_color;
 	double		shininess;
+	double		ambient_ratio;
 
 	if (light_ratio <= 0)
 		return (get_color("0,0,0"));
 	reflected_light = vector_sub(vector_mult_scalar(ray->hit_normal, 2 \
 				* vector_dot(ray->hit_normal, ray->hit_direction)), ray->hit_direction);
 	viewer_direction = vector_mult_scalar(ray->direction, -1);
-	shininess = 1;
-	specular_intensity = pow(vector_dot(reflected_light, viewer_direction), \
-			shininess);
-	if (specular_intensity < 0)
+	shininess = 50;
+	ambient_ratio = vector_dot(vector_unit(reflected_light), vector_unit(viewer_direction));
+	if (ambient_ratio < 0)
 		specular_intensity = 0;
+	else
+		specular_intensity = pow(ambient_ratio, shininess);
 	specular_color = apply_brightness(light.color, specular_intensity);
 	return (specular_color);
 }
