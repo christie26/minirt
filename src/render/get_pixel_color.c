87d4ab44444d3e57de2378e0a3bd4968127b7e9f
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-07-06 20:13:42 +0900

    norminette

--- src/render/get_pixel_color.c
+++ src/render/get_pixel_color.c
@@ -1,14 +1,5 @@
 #include "../../includes/minirt.h"
 
-// static t_color sky_color(t_ray ray, double vertical)
-// {
-//     t_color     color_rgb;
-//     double      ratio;
-
-//     ratio = (ray.direction.x + vertical) / (2 * vertical);
-//     color_rgb = mix_color(get_color("255,255,255"), get_color("30,144,255"), ratio);
-//     return (color_rgb);
-// }
 
 double	get_short_distance(t_list *object_list, t_ray *ray)
 {
@@ -34,40 +25,3 @@ double	get_short_distance(t_list *object_list, t_ray *ray)
 	}
 	return (short_distance);
 }
-
-// static t_color	get_color_rgb(t_ray *ray, t_data *data)
-// {
-// 	t_color			background_color;
-
-// 	background_color = sky_color(*ray, data->screen.view_height);
-// 	if (get_short_distance(data->object_list, ray) == INFINITY)
-// 		return (background_color);
-// 	return (apply_phong_model_to_all_lights(*data, ray));
-// }
-
-// void	get_pixel_color(t_data *data)
-// {
-// 	t_ray		ray;
-// 	t_color		color_rgb;
-// 	t_screen	screen;
-// 	int			color_hex;
-// 	double		u;
-// 	double		v;
-
-// 	screen = data->screen;
-// 	for (int j = WINDOW_HEIGHT - 1; j >= 0; --j)
-// 	{
-// 		for (int i = 0; i < WINDOW_WIDTH; ++i)
-// 		{
-// 			u = (double)(i) / 
-// 			v = (double)(j) / (WINDOW_HEIGHT - 1);
-// 			ray.origin = data->camera.coordinate;
-// 			ray.direction = get_ray_direction(screen.start_point,\
-// 				screen.horizontal, screen.vertical,data->camera.coordinate, \
-// 				u, v);
-// 			color_rgb = get_color_rgb(&ray, data);
-// 			color_hex = color_to_hex(color_rgb);
-// 			write_pixel_image(data, i, j, color_hex);
-// 		}
-// 	}
-// }
--- src/render/threads.c
+++ src/render/threads.c
@@ -1,35 +1,48 @@
 #include "../../includes/minirt.h"
 
+static t_render_thread	*get_data(t_render_thread *td, t_data *data, int index, \
+		t_render_thread thread_data[])
+{
+	td = &thread_data[index];
+	td->data = data;
+	td->thread_index = index;
+	return (td);
+}
+
+static void	get_screen_data(t_render_thread *td, int i, int j)
+{
+	int	block_width;
+	int	block_height;
+
+	block_width = WINDOW_WIDTH / THREADS_PER_DIMENSION;
+	block_height = WINDOW_HEIGHT / THREADS_PER_DIMENSION;
+	td->start_x = j * block_width;
+	td->end_x = (j + 1) * block_width;
+	td->start_y = i * block_height;
+	td->end_y = (i + 1) * block_height;
+	if (j == THREADS_PER_DIMENSION - 1)
+		td->end_x = WINDOW_WIDTH;
+	if (i == THREADS_PER_DIMENSION - 1)
+		td->end_y = WINDOW_HEIGHT;
+}
+
 void	make_threads(t_data *data, pthread_t threads[],
 		t_render_thread thread_data[])
 {
 	int				index;
-	int				block_width;
-	int				block_height;
 	int				i;
 	int				j;
 	t_render_thread	*td;
 
 	index = 0;
-	block_width = WINDOW_WIDTH / THREADS_PER_DIMENSION;
-	block_height = WINDOW_HEIGHT / THREADS_PER_DIMENSION;
 	i = 0;
 	while (i < THREADS_PER_DIMENSION)
 	{
 		j = 0;
 		while (j < THREADS_PER_DIMENSION)
 		{
-			td = &thread_data[index];
-			td->data = data;
-			td->start_x = j * block_width;
-			td->end_x = (j + 1) * block_width;
-			td->start_y = i * block_height;
-			td->end_y = (i + 1) * block_height;
-			td->thread_index = index;
-			if (j == THREADS_PER_DIMENSION - 1)
-				td->end_x = WINDOW_WIDTH;
-			if (i == THREADS_PER_DIMENSION - 1)
-				td->end_y = WINDOW_HEIGHT;
+			td = get_data(td, data, index, thread_data);
+			get_screen_data(td, i, j);
 			pthread_create(&threads[index], NULL, render_thread, td);
 			j++;
 			index++;
