2380417dae11a703d6cf79b12d291c86164b5248
yoonsele <yoonseo9800@gmail.com>
2023-07-11 14:46:29 +0900

    add EPSILON in blocked_object.c

--- src/render/blocked_object.c
+++ src/render/blocked_object.c
@@ -13,7 +13,7 @@ int	blocked_plane(t_plane plane, t_ray hit_to_light)
 	t = vector_dot(ray_to_plane, plane.vector) / denominator;
 	t_of_light = vector_length(get_vector_two_point(hit_to_light.origin, \
 				hit_to_light.light)) / vector_length(hit_to_light.direction);
-	return (t >= 0 && t < t_of_light);
+	return (t >= 0 && t <= t_of_light);
 }
 
 int	blocked_sphere(t_sphere sphere, t_ray hit_to_light)
@@ -26,19 +26,24 @@ int	blocked_sphere(t_sphere sphere, t_ray hit_to_light)
 				hit_to_light.light)) / vector_length(hit_to_light.direction);
 	if (info.discriminant < 0)
 		return (0);
-	return ((info.t_1 > 0 && info.t_1 < t_of_light) || \
-			(info.t_2 > 0 && info.t_2 < t_of_light));
+	return ((info.t_1 > 0 && info.t_1 <= t_of_light + EPSILON) || \
+			(info.t_2 > 0 && info.t_2 <= t_of_light + EPSILON));
 }
 
 int	blocked_cylinder(t_cylinder cylinder, t_ray hit_to_light)
 {
 	t_coordinate	hit_point;
+	double			t_of_light;
+	double			t_of_hit;
 
 	hit_point = get_closer_cylinder_point(cylinder, &hit_to_light);
 	if (hit_point.x == INFINITY && hit_point.y == INFINITY && hit_point.z == INFINITY)
 		return (0);
-	return (get_distance(hit_to_light.origin, hit_to_light.light) \
-					> get_distance(hit_to_light.origin, hit_point));
+	t_of_light = vector_length(get_vector_two_point(hit_to_light.origin, \
+				hit_to_light.light)) / vector_length(hit_to_light.direction);
+	t_of_hit = vector_length(get_vector_two_point(hit_to_light.origin, \
+				hit_point)) / vector_length(hit_to_light.direction);
+	return (t_of_hit <= t_of_light + EPSILON);
 }
 
 int	blocked_paraboloid(t_paraboloid paraboloid, t_ray hit_to_light)
