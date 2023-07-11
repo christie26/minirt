1e9aac2874fe2d73b43206034a824c84ac783e68
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-07-11 11:19:20 +0900

    using epsilon

--- src/utils/light_utils.c
+++ src/utils/light_utils.c
@@ -2,11 +2,11 @@
 
 void	fix_hit_to_light(t_ray *hit_to_light)
 {
-	hit_to_light->origin.x = hit_to_light->origin.x + 0.1 \
+	hit_to_light->origin.x = hit_to_light->origin.x + EPSILON \
 		* hit_to_light->direction.x;
-	hit_to_light->origin.y = hit_to_light->origin.y + 0.1 \
+	hit_to_light->origin.y = hit_to_light->origin.y + EPSILON \
 		* hit_to_light->direction.y;
-	hit_to_light->origin.z = hit_to_light->origin.z + 0.1 \
+	hit_to_light->origin.z = hit_to_light->origin.z + EPSILON \
 		* hit_to_light->direction.z;
 }
 
